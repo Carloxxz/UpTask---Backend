78cad7a289f269681fb9277b7bb4b4ac3f7c8b02
Carlos Martínez <carloxmz@gmail.com>
2024-06-12 17:28:49 -0600

    - Uso de morgna para los logs de peticiones - Solucion en schema task

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import { connectDB } from "./config/db"
 import router from "./routes/projectRoutes"
 import cors from "cors"
+import morgan from "morgan"
 import { corsConfig } from "./config/cors"
 
 dotenv.config()
@@ -11,7 +12,12 @@ connectDB()
 
 const app = express()
 app.use(cors(corsConfig))
+
+// Leer datos de formularios
 app.use(express.json())
+
+// Login
+app.use(morgan('dev'))
 
 // Routes
 app.use('/api/projects', router)
